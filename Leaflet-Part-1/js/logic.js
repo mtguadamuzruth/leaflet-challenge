5a2dcfef95162939104738d7f6216ddf7d9c946f
mtguadamuzruth <travisguadamuzruth92@gmail.com>
2023-10-18 16:58:21 -0400

    completed HW assigment.

--- Leaflet-Part-1/js/logic.js
+++ Leaflet-Part-1/js/logic.js
@@ -1,5 +1,5 @@
 // creating map
-var map = L.map('map').setView([37.3, -114.62], 5);
+var map = L.map('map').setView([0, -45], 3);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -8,31 +8,42 @@ const colors = ['red', 'yellow', 'green']
 fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(res => res.json()).then(data => {
     //making markers 
     for (let x of data.features) {
-        console.log(x.geometry.coordinates)
-        var circle = L.circle([x.geometry.coordinates[1], x.geometry.coordinates[0]], {
-            color: colors[Math.floor(Math.log(x.geometry.coordinates[2]))],
-            fillColor: colors[Math.floor(Math.log(x.geometry.coordinates[2]))],
-            fillOpacity: .5,
-            radius: 5000 * x.properties.mag
-        }).addTo(map);
+        let mag = x.properties.mag;
+        let place = x.properties.place;
+        let lon = x.geometry.coordinates[0];
+        let lat = x.geometry.coordinates[1];
+        let depth = x.geometry.coordinates[2];
+        let date = new Date(x.properties.time).toLocaleString()
+
+        var circle = L.circle([lat, lon], {
+            color: 'black',
+            weight: 1,
+            fillColor:
+                depth < 10 ? 'green' :
+                    depth < 30 ? 'lime' :
+                        depth < 50 ? 'yellow' :
+                            depth < 70 ? 'orange' :
+                                depth < 90 ? 'darkorange' : 'red',
+            fillOpacity: .75,
+            radius: 25000 * x.properties.mag
+        }).bindPopup(`<h3>${place}<br>${date}<br>Magnitude: ${mag}</h3>`).addTo(map);
     }
-    var legend = L.control({ position: 'bottomright' });
+});
 
-    legend.onAdd = function (map) {
+var legend = L.control({ position: 'bottomright' });
 
-        var div = L.DomUtil.create('div', 'info legend'),
-            mag = [0, 10, 100],
-            labels = [];
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML += `
+            <div> 10-10</div>        
+            <div> 10 - 30</div>        
+            <div> 30 - 50 </div>        
+            <div> 50 - 70 </div>        
+            <div> 70 - 90 </div>        
+            <div> 90+ </div>`;
+    return div;
+}
 
-        // loop through our density intervals and generate a label with a colored square for each interval
-        for (var i = 0; i < mag.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + colors[Math.floor(Math.log(mag[i]))] + '"></i> ' +
-                mag[i] + (mag[i + 1] ? '&ndash;' + mag[i + 1] + '<br>' : '+');
-        }
-        return div;
-    };
+legend.addTo(map);
 
-    legend.addTo(map);
-})
 
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# leaflet-challenge
+# leaflet challege
+-Website: [website](travisguadamuzruth92Gmail.com)
+
+
+## Description
+weekly earthquake 
+
+
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+
+- [License](#license)
+- [Features](#features)
+
+- [Contact](#contact)
+
+## Installation
+html javascript, CSS  
+
+## Usage
+run the application to see the weekly earthquakes 
+
+
+
+
+
+## Features
+provides weekly information about earthquakes 
+
+
+
+## Contact
+If there are any questions of concerns, I can be reached at:
+##### [github: mguadamuzruth](https://github.com/mguadamuzruth)
+##### [email: travisguadamuzruth92Gmail.com](mailto:travisguadamuzruth92Gmail.com)
